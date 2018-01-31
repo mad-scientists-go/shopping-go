33231e8a5231e50aa60f05770df290e4c2d6088b
ray <raytorres280@gmail.com>
2018-01-31 10:17:51 -0500

    orders

---
+++ server/api/orders.js
@@ -0,0 +1,65 @@
+const router = require('express').Router()
+const {User, Order, LineItem, Product} = require('../db/models')
+
+module.exports = router
+
+//all orders
+router.get('/', (req, res, next) => {
+  Order.findAll()
+    .then(users => res.json(users))
+    .catch(next)
+})
+
+//search for orders by search fields
+router.post('/', (req, res, next) => {
+  Order.findAll({
+    where: {
+			...req.body
+		},
+		include: [
+			User,
+			{
+				model: LineItem,
+				include: [Product]
+			}
+		]
+  })
+    .then(users => res.json(users))
+    .catch(next)
+})
+
+//completed orders
+router.get('/completed', (req, res, next) => {
+  Order.findAll({
+    where: {
+				status: 'paid'
+		},
+		include: [
+			User,
+			{
+				model: LineItem,
+				include: [Product]
+			}
+		]
+  })
+    .then(users => res.json(users))
+    .catch(next)
+})
+
+//unpaid or pending orders
+router.get('/unpaid', (req, res, next) => {
+  Order.findAll({
+    where: {
+				status: 'pending'
+		},
+		include: [
+			User,
+			{
+				model: LineItem,
+				include: [Product]
+			}
+		]
+  })
+    .then(users => res.json(users))
+    .catch(next)
+})
