d3740a977db7499e0558865e8d7ca5d16a52a6a9
ray <raytorres280@gmail.com>
2018-02-02 16:39:39 -0500

    fixed lineitem route

--- server/api/lineItems.js
+++ server/api/lineItems.js
@@ -1,39 +1,42 @@
 const router = require('express').Router()
-const {LineItem, Order} = require('../db/models')
+const {LineItem, Product, Order} = require('../db/models')
 module.exports = router
 
-router.post('/', (req, res, next) => { // order id , product id , quantity.
-  console.log(req.body, 'reqbody')
-LineItem
-.findOrCreate({
-  where: {orderId: req.body.orderId, productId: req.body.productId}
-})
-.then(([lineItem, created])=>{
-  console.log("hitting the route for line items")
-  // if (!created){
-  //   console.log('findOrCreate', req.body)
-    //lineItem.qty = lineItem.qty + req.body.qty // updating the quantity
-    lineItem.increment('qty', {by: req.body.quantity } )
-  // }
-})
-.then(
-  Order
-  .findOne({
+router.post('/', (req, res, next) => { // order id , product id , price and quantity.
+  const { orderId, productId, productName,  qty } = req.body
+  LineItem
+  .findOrCreate({
     where: {
-      order: {
-        id: req.body.orderId
-      }
+      orderId,
+      productId
+    }, defaults: {
+      orderId,
+      productId,
+      productName,
+      qty
     }
-}))})
-.then(order => {
-  console.log('ORDER FROM LINE ITEM', order)
-  res.json(order)
+  })
+  .then(([lineItem, created]) => {
+    if (!created){
+      console.log('findOrCreate', req.body)
+      return lineItem.update({ qty: req.body.qty + lineItem.qty })
+      .then(() => {
+        //if it was updated
+        Product.increment('inventory', { by: -req.body.qty, where: { id: lineItem.productId } })
+      })
+      //lineItem.qty = lineItem.qty + req.body.qty // updating the quantity
+      // lineItem.save()
+    }
+  })
+  // .then(res=>res.data)
+  .then(() => {
+    Order.findById(orderId).then(order => res.json(order))
+  })
+  .catch(next)
+    // .create(req.body)
+    // .then(item => res.json(item))
+    // .catch(next)
 })
-.catch(next)
-  // .create(req.body)
-  // .then(item => res.json(item))
-  // .catch(next)
-
 
 // put to line item
 router.delete('/', (req, res, next) => {
