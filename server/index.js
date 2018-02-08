ca80f7aa53a9e04fbca12022b0118703954c60fa
ray <raytorres280@gmail.com>
2018-02-08 13:19:30 -0500

    hello

--- server/index.js
+++ server/index.js
@@ -56,6 +56,7 @@ const createApp = () => {
   // // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
+      console.log(req.path)
       const err = new Error('Not found')
       err.status = 404
       next(err)
